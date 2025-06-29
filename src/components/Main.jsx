4818324d3c85ba0fce3c206ca18ca54a73af3438
Saransh-Basu-01 <saranshbasu@gmail.com>
2025-06-29 21:28:48 +0545

    end

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Die from './Die'
 import { useState } from 'react'
 import {nanoid} from "nanoid"
+import Confetti from 'react-confetti'
+import { useWindowSize } from '@react-hook/window-size'
+
 const Main = () => {
   function generatedAllDice(){
     const arr=[]
@@ -14,17 +17,23 @@ const Main = () => {
     return arr
   }
  function rolldice() {
-        setDice(oldDice => oldDice.map(die => 
-            die.isHeld ?
-                die :
-                { ...die, value: Math.ceil(Math.random() * 6) }
-        ))
+  if(!gameWon){
+    setDice(oldDice => oldDice.map(die => 
+        die.isHeld ?
+            die :
+            { ...die, value: Math.ceil(Math.random() * 6) }
+    ))
     }
+    else{
+      setDice(generatedAllDice())
+    }
+  }
   function hold(id){
     setDice(prevDice=>prevDice.map(die=>die.id === id?{...die,isHeld: !die.isHeld}:die))
   }
-  const [dice,setDice]=useState(generatedAllDice())
-  
+
+  const [dice,setDice]=useState( () => generatedAllDice())
+  const [width, height] = useWindowSize()
   const gameWon=dice.every(die=>die.isHeld)&&dice.every(die=>die.value===dice[0].value)
   
   const diceElements=dice.map(dieObj=> 
@@ -43,11 +52,20 @@ const Main = () => {
       <div className='container flex items-center justify-center h-screen p-4 mx-auto bg-gray-800'>
         <div className='bg-white h-[80vh] w-full max-w-[80vh] sm:h-[500px] shadow-xl rounded-3xl flex flex-col justify-center items-center'>
         <h1 className="title">Tenzies</h1>
-        <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        <p className="mt-2 text-center text-gray-600 text-md sm:text-lg">
+        Roll until all dice are the same. Click each die to freeze it.
+        </p>
         <div className="grid grid-cols-5 gap-4 mt-2">
           {diceElements}
         </div>
-        <button className='mx-auto mt-10 text-4xl font-bold text-white bg-blue-900 w-25 h-15 rounded-2xl hover:bg-blue-950' onClick={rolldice}>{gameWon?"New ":"Roll"}</button>
+        <button 
+         className={`mx-auto mt-10 text-4xl font-bold text-white w-25 h-15   rounded-2xl hover:scale-105 duration-150
+        ${gameWon ? 'bg-green-600 hover:bg-green-700' : 'bg-blue-900  hover:bg-blue-950'}`}
+       onClick={rolldice}>
+      {gameWon ? "New" : "Roll"}
+       </button>
+        {gameWon && <Confetti width={width} height={height} />}
+        {gameWon?<p className='mt-2 text-2xl font-bold italic'>YOU HAVE WON!!</p> :null}
           </div>
       </div>
     </>
